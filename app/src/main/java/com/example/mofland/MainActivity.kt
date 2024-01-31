94a19b1956b6c5993edc4476908a54d5c3d8e6a0
Killian <killianm2259@gmail.com>
2024-01-31 10:08:21 +0100

    orientation + navigation : hidden

--- app/src/main/java/com/example/mofland/MainActivity.kt
+++ app/src/main/java/com/example/mofland/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mofland
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,8 @@ import com.example.mofland.ui.theme.MoflandTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        actionBar?.show()
         super.onCreate(savedInstanceState)
         setContent {
             MoflandTheme {
