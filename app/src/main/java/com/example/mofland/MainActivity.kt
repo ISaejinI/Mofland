784ff37be089ebdebbbcac6ebedb2733bfa99b3f
Killian <killianm2259@gmail.com>
2024-01-31 10:25:49 +0100

    preview size : landscape

--- app/src/main/java/com/example/mofland/MainActivity.kt
+++ app/src/main/java/com/example/mofland/MainActivity.kt
@@ -40,7 +40,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true, showSystemUi = true, device = "spec:width=411dp,height=891dp,dpi=420,isRound=false,chinSize=0dp,orientation=landscape")
 @Composable
 fun GreetingPreview() {
     MoflandTheme {
