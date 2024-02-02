766c4b08f57c68e87977ed4445e6db594891020a
Lou-Anne <saejinmx@gmail.com>
2024-02-02 15:50:23 +0100

    Clicker

--- app/src/main/java/com/example/mofland/MainActivity.kt
+++ app/src/main/java/com/example/mofland/MainActivity.kt
@@ -3,12 +3,22 @@ package com.example.mofland
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mofland.ui.theme.MoflandTheme
 
@@ -17,30 +27,42 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MoflandTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                        ClickButton()
                 }
             }
         }
     }
 }
 
+
+//@Composable
+//fun TotalRessources(){
+
+//}
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ClickButton(modifier: Modifier = Modifier){
+    var counterbois by remember { mutableIntStateOf(0) }
+    Column {
+        Image(
+            painter = painterResource(R.drawable.forest),
+            contentDescription = null,
+            modifier=Modifier.clickable { counterbois += 1
+            })
+        Text(text = "Clicks : $counterbois")
+
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MoflandTheme {
-        Greeting("Android")
+
     }
 }
