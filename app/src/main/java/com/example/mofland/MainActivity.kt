60c040e7e56bf4f174648c46d214c34be4686d19
Killian <killianm2259@gmail.com>
2024-03-12 12:55:40 +0100

    remove android bar

--- app/src/main/java/com/example/mofland/MainActivity.kt
+++ app/src/main/java/com/example/mofland/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mofland
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -28,6 +29,10 @@ import kotlin.math.ceil
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
         super.onCreate(savedInstanceState)
         setContent {
             MoflandTheme {
